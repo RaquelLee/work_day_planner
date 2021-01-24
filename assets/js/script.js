b38686b314bd520d5b1b80ebe2293f836f0ef98c
rsco <RaquelLee@users.noreply.github.com>
2021-01-23 23:03:07 -0500

    for loop looping through user inputs and hour values to generate page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,18 @@ var currentDay = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").text(currentDay);
 
 var currentHr = moment().format('HH');
-var currentHrInt = parseInt(currentHr)
+var currentHrInt = parseInt(currentHr);
 var elTextArea = $("textarea");
+
+for(var i = 0; i < elTextArea.length; i++){
+    var $this = $(elTextArea[i]);
+    var elTime = parseInt($this.attr("value"));
+    var current = localStorage.getItem(elTime);
+    $this.val(current);
+    if (elTime < currentHrInt) {
+        $this.addClass("past");
+    } else if (elTime === currentHrInt) {
+        $this.addClass("present");
+    } else {
+        $this.addClass("future");
+}};
