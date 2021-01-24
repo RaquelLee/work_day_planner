9b11fd5d588b8d61068dfb559dced0646d17a412
rsco <RaquelLee@users.noreply.github.com>
2021-01-23 22:01:00 -0500

    data used for current time and user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
 var currentDay = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").text(currentDay);
+
+var currentHr = moment().format('HH');
+var currentHrInt = parseInt(currentHr)
+var elTextArea = $("textarea");
